002d99f336103c53f43994b16207f24f91bf536a
scheckland <scheckland@assetscape.co.uk>
2026-07-14 09:10:45 +0100

    Alter base url

--- app1/PhotoGlobe/src/api/client.js
+++ app1/PhotoGlobe/src/api/client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create a configured instance of Axios
 const apiClient = axios.create({
   // eslint-disable-next-line no-undef
-  baseURL: import.meta.env.VITE_API_BASE_URL_LOCAL || "/local-network",
+  baseURL: import.meta.env.VITE_BASE_URL_LOCAL,
   timeout: 3000, // 10 seconds timeout
   headers: {
     "Content-Type": "application/json",

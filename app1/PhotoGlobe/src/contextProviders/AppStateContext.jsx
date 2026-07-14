ad72d9d944663b78b3181aa96a4bb15c3d06dbf7
scheckland <scheckland@assetscape.co.uk>
2026-07-14 10:01:00 +0100

    change url base logic

--- app1/PhotoGlobe/src/contextProviders/AppStateContext.jsx
+++ app1/PhotoGlobe/src/contextProviders/AppStateContext.jsx
@@ -15,17 +15,8 @@ export function AppStateProvider({ children }) {
       // 1.5-second timeout so your app loads quickly even if local fails
       const timeoutId = setTimeout(() => controller.abort(), 1500);
 
-      try {
-        // Try hitting a small asset or health endpoint on your local server
-        await fetch(`/local-network/uploads/hello`, {
-          method: "GET",
-          signal: controller.signal,
-        });
-
-        console.log(
-          "Local image server detected. Setting global path to Local.",
-        );
-
+      if (import.meta.env.DEV) {
+        console.log("The application is running in development mode.");
         // 1. Set image base state
         setImgBaseUrl(`/local-network/uploads`);
 
@@ -33,19 +24,42 @@ export function AppStateProvider({ children }) {
         const localApi = `/local-network/api`;
         setApiBaseUrl(localApi);
         setupAxiosInterceptors(localApi);
-      } catch (error) {
-        console.warn(
-          "Local image server unreachable. Setting global path to Public WAN.",
-        );
-        setImgBaseUrl(`${import.meta.env.VITE_BASE_URL_PUBLIC}/uploads`);
-
-        // 3. Set the fallback string AND bind it to Axios
-        const publicApi = `${import.meta.env.VITE_BASE_URL_PUBLIC}/api`;
-        setApiBaseUrl(publicApi);
-        setupAxiosInterceptors(publicApi);
-      } finally {
-        clearTimeout(timeoutId);
         setLoading(false);
+      } else {
+        console.log("The application is running in PRODUCTION mode.");
+
+        try {
+          // Try hitting a small asset or health endpoint on your local server
+          await fetch(`${import.meta.env.VITE_BASE_URL_LOCAL}/uploads/hello`, {
+            method: "GET",
+            signal: controller.signal,
+          });
+
+          console.log(
+            "Local image server detected. Setting global path to Local.",
+          );
+
+          // 1. Set image base state
+          setImgBaseUrl(`${import.meta.env.VITE_BASE_URL_LOCAL}/uploads`);
+
+          // 2. Set the state AND instantly bind it to Axios
+          const localApi = `${import.meta.env.VITE_BASE_URL_LOCAL}/api`;
+          setApiBaseUrl(localApi);
+          setupAxiosInterceptors(localApi);
+        } catch (error) {
+          console.warn(
+            "Local image server unreachable. Setting global path to Public WAN.",
+          );
+          setImgBaseUrl(`${import.meta.env.VITE_BASE_URL_PUBLIC}/uploads`);
+
+          // 3. Set the fallback string AND bind it to Axios
+          const publicApi = `${import.meta.env.VITE_BASE_URL_PUBLIC}/api`;
+          setApiBaseUrl(publicApi);
+          setupAxiosInterceptors(publicApi);
+        } finally {
+          clearTimeout(timeoutId);
+          setLoading(false);
+        }
       }
     };
 

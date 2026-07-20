52a8176a26beb492df35080181ab461f20c1c7cd
simcheckland-ship-it <sim.checkland@outlook.com>
2026-07-20 15:10:03 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     proxmox = {
@@ -10,9 +11,11 @@ terraform {
 variable "proxmox_endpoint" { type = string }
 variable "proxmox_token"    { type = string }
 variable "server_inventory" { type = map(any) }
-
-# 1. Declare the incoming SSH Key variable
-# variable "ssh_public_key"   { type = string }
+variable "server_passwords" { 
+  type    = map(string)
+  default = {}
+}
+variable "ssh_public_key"   { type = string }
 
 provider "proxmox" {
   endpoint  = var.proxmox_endpoint
@@ -37,9 +40,9 @@ resource "proxmox_virtual_environment_vm" "docker_hosts" {
 
   initialization {
     # 2. Inject your secure public key into the VM user account
-    # user_account {
-    #   keys = [var.ssh_public_key]
-    #}
+    user_account {
+       keys = [var.ssh_public_key]
+    }
 
     ip_config {
       ipv4 {

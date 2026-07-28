251eec17937fe6e5d07b8c8e45734e6e688373da
scheckland <scheckland@assetscape.co.uk>
2026-07-28 13:01:27 +0100

    Update api url

--- app-iphone/app-iphone/src/App.jsx
+++ app-iphone/app-iphone/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
 
     try {
       // 3. Send POST request to your Web API
-      const response = await fetch("https://192.168.0.200/api/Photos/upload", {
+      const response = await fetch("https://82.8.217.2/api/Photos/upload", {
         method: "POST",
         body: formData,
         // Note: Do NOT manually set Content-Type header.
